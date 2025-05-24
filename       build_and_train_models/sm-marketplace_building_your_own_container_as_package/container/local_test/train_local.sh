019e1a509f4c94e018b368385f427db81b228a2e
Vikash Gupta <write2vikash@gmail.com>
2025-05-24 07:16:05 +0000

    tested local

---       build_and_train_models/sm-marketplace_building_your_own_container_as_package/container/local_test/train_local.sh
+++       build_and_train_models/sm-marketplace_building_your_own_container_as_package/container/local_test/train_local.sh
@@ -5,7 +5,8 @@ image=$1
 mkdir -p test_dir/model
 mkdir -p test_dir/output
 
-rm test_dir/model/*
-rm test_dir/output/*
+rm -f test_dir/model/*
+rm -f test_dir/output/*
 
-docker run -v $(pwd)/test_dir:/opt/ml --rm ${image} train
+
+docker run -v ./test_dir:/opt/ml --rm ${image} train
